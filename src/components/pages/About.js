d7fce68f2956996f72f0297a0a6fd6985e57503d
SoGGod <apoudel996@gmail.com>
2021-09-20 15:13:50 +0545

    monday 20th 3pm

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -20,7 +20,8 @@ function About() {
                  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
                  consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
                  qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                 sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                 sed quia non numquam eius modi
+                  tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
 
                 </div>
               </div>
